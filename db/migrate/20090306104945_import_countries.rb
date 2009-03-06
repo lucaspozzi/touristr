380095f2e4b1bb9c4d930886da82fa7416a1d797
arnaud <arnaud@touristr.com>
2009-03-06 12:56:18 +0100

    Fix wrong migration to import countries

--- db/migrate/20090306104945_import_countries.rb
+++ db/migrate/20090306104945_import_countries.rb
@@ -3,8 +3,10 @@ class ImportCountries < ActiveRecord::Migration
     sqlString = "LOAD DATA LOCAL INFILE '#{RAILS_ROOT}/db/data/geonames_countryInfo.txt' INTO TABLE countries"
     sqlString += " FIELDS TERMINATED BY '\t'"
     sqlString += " (iso, iso3, iso_numeric, fips, country, capital, area_sqkm, population, continent, tld, currency_code, currency_name, phone, postal_code_format, postal_code_regex, languages, geoname_id, neighbours, equivalent_fips_code)"
+    execute sqlString  
   end
 
   def self.down
+    execute "delete from countries"
   end
 end
