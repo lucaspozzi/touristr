c4d62dc61a3f0535fdc28c148b7f4c51d7a142fe
steve <steve@lesseverything.com>
2009-03-08 12:53:29 -0400

    all tests passing

--- test/unit/todo_test.rb
+++ test/unit/todo_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class TodoTest < ActiveSupport::TestCase
-   should_have_many :trip_items, :dependent=>:destroy
+   should_have_one :trip_item, :dependent=>:destroy
    should_validate_presence_of :title
 
    
