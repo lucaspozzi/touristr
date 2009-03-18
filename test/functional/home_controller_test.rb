28ae35fff95e1c151530d1de0f556c751f5ab475
arnaud <arnaud@touristr.com>
2009-03-18 14:39:54 +0100

    Removed test as per Conor request as new layout doesn't show trip on homepage

--- test/functional/home_controller_test.rb
+++ test/functional/home_controller_test.rb
@@ -13,16 +13,17 @@ class HomeControllerTest < ActionController::TestCase
   # end
 
 
-  should "show a link to their trip when logged in" do
-    user = create_user
-    get :index, {}, {:user=>user.id}
-    person = assigns['p']
-    assert !person.new_record?
-    assert person.current_trip
-    assert_equal user.person, person
-    assert_match "My Trip</a>" , @response.body
-    assert_nil@response.cookies['_touristr_person']
-  end
+# Conor: not relevant anymore as new layout
+  # should "show a link to their trip when logged in" do
+  #   user = create_user
+  #   get :index, {}, {:user=>user.id}
+  #   person = assigns['p']
+  #   assert !person.new_record?
+  #   assert person.current_trip
+  #   assert_equal user.person, person
+  #   assert_match "My Trip</a>" , @response.body
+  #   assert_nil@response.cookies['_touristr_person']
+  # end
 
 
 end
