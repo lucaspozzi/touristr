d0abd4fdc86fe500794c6b930df6149b456bc50a
adaron <arnaud@touristr.com>
2009-04-17 14:54:11 +0200

    shows destination airport for ezrez flights search

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -17,12 +17,13 @@ class FlightsController < ApplicationController
 
     # we can either have a IATA code (String) or an array of Airports
     if arr_airports.instance_of?(String)
+      @arr_airport = arr_airports
       @flights = ez_flight.flight_search(:Cabin => "coach", :FromAirport => @dep_airport, :ToAirport => arr_airports, :DepartureDateTime => @dep_date, :ReturnDateTime=> @ret_date)
     else
       arr_airports.each do |arr_airp|
         @arr_airport = arr_airp.iata_code
         @flights = ez_flight.flight_search(:Cabin => "coach", :FromAirport => @dep_airport, :ToAirport => arr_airp.iata_code, :DepartureDateTime => @dep_date, :ReturnDateTime=> @ret_date)
-        break if !@flights.nil?
+        break if !@flights.nil? # Stop if we have found a flight to an airport close to the destination
       end
     end
   end
